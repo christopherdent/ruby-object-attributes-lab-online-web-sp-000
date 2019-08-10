4542ae23bf95475b87e94ca56e7f22e9f3a257c4
Christopher Dent <christopherdent01@gmail.com>
2019-08-10 03:47:01 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,14 +2,20 @@ class Person
   
   
 def name
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  @name
+end 
+
+def name=(person_name)
+  @name = person_name
+end 
+
+
+def job
+  @job 
+end 
+
+def job=(his_job)
+  @job = his_job
+end 
+
 end 
