a557d5a0bd5d64a0d994324805942b8123d49093
Christopher Dent <christopherdent01@gmail.com>
2019-08-10 03:44:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,19 @@
 class Dog 
-  def name 
-    @name = name 
-  end
+ 
   
   def name
     @name 
   end 
   
   def name=(new_name)
-  @name = new_name 
-end 
+    @name = new_name 
+  end 
+
+
+
+
+
+
+
 end 
     
